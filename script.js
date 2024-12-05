64fbf0b49f4eb696d43c95e2354debfd48c86959
VladimirB <vlbelevich@yandex.by>
2024-12-05 18:36:17 +0300

    Add function to create first operand

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let firstNumber = 0;
+let firstNumber = "0";
 
 const screen = document.querySelector(".screen");
 screen.textContent = firstNumber;
 
 const reset = document.querySelector(".ac");
 reset.addEventListener("click", () => {
+  firstNumber = "0";
   screen.textContent = "0";
 });
 
@@ -31,34 +32,54 @@ const add = document.querySelector(".add");
 add.addEventListener("click", () => {});
 
 const zero = document.querySelector(".zero");
-zero.addEventListener("click", () => {});
+zero.addEventListener("click", () => {
+  createNumber("0");
+});
 
 const one = document.querySelector(".one");
-one.addEventListener("click", () => {});
+one.addEventListener("click", () => {
+  createNumber("1");
+});
 
 const two = document.querySelector(".two");
-two.addEventListener("click", () => {});
+two.addEventListener("click", () => {
+  createNumber("2");
+});
 
 const three = document.querySelector(".three");
-three.addEventListener("click", () => {});
+three.addEventListener("click", () => {
+  createNumber("3");
+});
 
 const four = document.querySelector(".four");
-four.addEventListener("click", () => {});
+four.addEventListener("click", () => {
+  createNumber("4");
+});
 
 const five = document.querySelector(".five");
-five.addEventListener("click", () => {});
+five.addEventListener("click", () => {
+  createNumber("5");
+});
 
 const six = document.querySelector(".six");
-six.addEventListener("click", () => {});
+six.addEventListener("click", () => {
+  createNumber("6");
+});
 
 const seven = document.querySelector(".seven");
-seven.addEventListener("click", () => {});
+seven.addEventListener("click", () => {
+  createNumber("7");
+});
 
 const eight = document.querySelector(".eight");
-eight.addEventListener("click", () => {});
+eight.addEventListener("click", () => {
+  createNumber("8");
+});
 
 const nine = document.querySelector(".nine");
-nine.addEventListener("click", () => {});
+nine.addEventListener("click", () => {
+  createNumber("9");
+});
 
 const plusMinus = document.querySelector(".plusMinus");
 plusMinus.addEventListener("click", () => {});
@@ -68,3 +89,12 @@ dot.addEventListener("click", () => {});
 
 const equal = document.querySelector(".equal");
 equal.addEventListener("click", () => {});
+
+function createNumber(num) {
+  if (firstNumber == 0) {
+    firstNumber = num;
+  } else {
+    firstNumber += num;
+  }
+  screen.textContent = firstNumber;
+}
